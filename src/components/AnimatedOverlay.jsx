0d2a12f19bc4df63d09d1c7cdca6af18bc50fff8
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2025-02-05 19:51:58 -0300

    fixed minimum bug on transition

--- src/components/AnimatedOverlay.jsx
+++ src/components/AnimatedOverlay.jsx
@@ -1,81 +1,82 @@
-import { useEffect, useRef } from "preact/hooks";
-import MODAL_STATES from "../constants/modalState";
+import { useEffect, useRef } from 'preact/hooks';
+import MODAL_STATES from '../constants/modalState';
 
 const AnimatedOverlay = ({
-  className,
-  extraClassNames,
-  modalState,
-  isAllwaysVisible,
-  onClose,
-  children,
+	className,
+	extraClassNames,
+	modalState,
+	isAllwaysVisible,
+	onClose,
+	children,
 }) => {
-  const overlayRef = useRef(null);
+	const overlayRef = useRef(null);
 
-  useEffect(() => {
-    if (isAllwaysVisible) return;
-    if (!overlayRef) return;
+	useEffect(() => {
+		if (isAllwaysVisible) return;
+		if (!overlayRef) return;
 
-    let timerID;
-    const overlayContainer = overlayRef.current;
+		let timerID;
+		const overlayContainer = overlayRef.current;
 
-    const transitionAction = () => {
-      if (modalState.value === MODAL_STATES.toFinal) {
-        modalState.value = MODAL_STATES.final;
-        return;
-      }
-      if (modalState.value === MODAL_STATES.toInitial) {
-        overlayContainer.classList.add(MODAL_STATES.initial);
-        modalState.value = MODAL_STATES.initial;
-        if (onClose) {
-          onClose();
-        }
-        return;
-      }
-    };
+		const transitionAction = (ev) => {
+			if (ev.target !== overlayContainer) return;
+			if (modalState.value === MODAL_STATES.toFinal) {
+				modalState.value = MODAL_STATES.final;
+				return;
+			}
+			if (modalState.value === MODAL_STATES.toInitial) {
+				overlayContainer.classList.add(MODAL_STATES.initial);
+				modalState.value = MODAL_STATES.initial;
+				if (onClose) {
+					onClose();
+				}
+				return;
+			}
+		};
 
-    overlayContainer.addEventListener("transitionend", transitionAction);
+		overlayContainer.addEventListener('transitionend', transitionAction);
 
-    if (modalState.value === MODAL_STATES.toFinal) {
-      overlayContainer.classList.remove(extraClassNames[MODAL_STATES.initial]);
-      timerID = setTimeout(() => {
-        overlayContainer.classList.add(extraClassNames[MODAL_STATES.final]);
-      }, 10);
-      return;
-    }
+		if (modalState.value === MODAL_STATES.toFinal) {
+			overlayContainer.classList.remove(extraClassNames[MODAL_STATES.initial]);
+			timerID = setTimeout(() => {
+				overlayContainer.classList.add(extraClassNames[MODAL_STATES.final]);
+			}, 10);
+			return;
+		}
 
-    if (modalState.value === MODAL_STATES.toInitial) {
-      overlayContainer.classList.remove(extraClassNames[MODAL_STATES.final]);
-      return;
-    }
+		if (modalState.value === MODAL_STATES.toInitial) {
+			overlayContainer.classList.remove(extraClassNames[MODAL_STATES.final]);
+			return;
+		}
 
-    return () => {
-      clearTimeout(timerID);
-      overlayContainer.removeEventListener("transitionend", transitionAction);
-    };
-  }, [
-    isAllwaysVisible,
-    modalState,
-    modalState.value,
-    extraClassNames,
-    onClose,
-  ]);
+		return () => {
+			clearTimeout(timerID);
+			overlayContainer.removeEventListener('transitionend', transitionAction);
+		};
+	}, [
+		isAllwaysVisible,
+		modalState,
+		modalState.value,
+		extraClassNames,
+		onClose,
+	]);
 
-  let extraClass;
+	let extraClass;
 
-  if (!isAllwaysVisible) {
-    extraClass =
-      modalState.value === MODAL_STATES.initial
-        ? extraClassNames[MODAL_STATES.initial]
-        : "";
-  } else {
-    extraClass = extraClassNames[MODAL_STATES.final];
-  }
+	if (!isAllwaysVisible) {
+		extraClass =
+			modalState.value === MODAL_STATES.initial
+				? extraClassNames[MODAL_STATES.initial]
+				: '';
+	} else {
+		extraClass = extraClassNames[MODAL_STATES.final];
+	}
 
-  return (
-    <div class={`${className} ${extraClass}`} ref={overlayRef}>
-      {children}
-    </div>
-  );
+	return (
+		<div class={`${className} ${extraClass}`} ref={overlayRef}>
+			{children}
+		</div>
+	);
 };
 
 export default AnimatedOverlay;
--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,14 +1,14 @@
 const Dialog = ({ onAcept, onCancel }) => {
-  return (
-    <div>
-      <p>Esta seguro que quiere aceptar</p>
-      <button type="button" onClick={onAcept}>
-        aceptar
-      </button>
-      <button type="button" onClick={onCancel}>
-        cancelar
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<p>Esta seguro que quiere aceptar</p>
+			<button type='button' onClick={onAcept}>
+				aceptar
+			</button>
+			<button type='button' onClick={onCancel}>
+				cancelar
+			</button>
+		</div>
+	);
 };
 export default Dialog;
